cab0d73dc96b7531f4d19348fb3d4da64a19fe2d
Bernita Chen <bernitachen@Bernitas-MacBook-Air.local>
2023-04-11 21:28:06 -0700

    homework 2

--- question-1.sql
+++ question-1.sql
@@ -1,4 +1,4 @@
 /* (question-1.sql) Show the customer's full name, Invoice ID, Date of the invoice and billing country of customers who are from Brazil. */
 SELECT FirstName, LastName, InvoiceId, InvoiceDate, BillingCountry 
 FROM customers, invoices
-WHERE customers.CustomerId = invoices.CustomerId AND Country = 'Brazil';
+WHERE customers.CustomerId = invoices.CustomerId AND invoices.BillingCountry = 'Brazil';
